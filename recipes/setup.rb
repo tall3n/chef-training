f228e5b2aa6f63a5c500c8f74054ae5f38fbb673
Tyler Allen <tyler@tallen.io>
2018-05-21 02:02:07 +0000

    Initial Commit

---
+++ recipes/setup.rb
@@ -0,0 +1,20 @@
+package 'tree' do 
+  action :install
+end
+
+
+package 'ntp' do
+ action :install
+end
+
+package 'git' do
+ action :install
+end
+
+file '/etc/motd' do
+  content "Tyler is learning chef!"
+  owner 'root'
+  group 'root'
+end
+
+
